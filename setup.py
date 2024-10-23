80cf360bf0105e9ee67e0f7583b96ecc7152bc22
Shrishailya Chavan <115857053+ShrishailyaChavan@users.noreply.github.com>
2024-10-23 13:03:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
         'playwright', 
         'sentence-transformers',  # Assuming these are your dependencies
     ],
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Shrishailya chavan',
+    author_email='shrichavan19@gmail.com',
     description='A package for web scraping and data extraction.',
     keywords='web scraping, data extraction, AI',
     url='https://example.com/RufusPackage'
